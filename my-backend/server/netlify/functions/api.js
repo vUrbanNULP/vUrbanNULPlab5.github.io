b2822385a59d8842fe15d73d1e0fcb2158fc258f
vUrban <181973728+vUrbanNULP@users.noreply.github.com>
2025-04-07 21:26:59 +0300

    Delete my-backend/server directory

--- my-backend/server/netlify/functions/api.js
+++
@@ -1,6 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../server/server');
-
-module.exports.handler = serverless(app);
-
-console.log('Netlify function api.js initialized.');
